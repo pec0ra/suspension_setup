b7c09e24ae9b6dde4d2d9d26464ae1c46b34f488
pec0ra <basile.maret@proton.me>
2026-05-06 12:54:43 +0200

    Fix screenshot

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:integration_test/integration_test_driver_extended.dart';
@@ -13,7 +12,7 @@ Future<void> main() => integrationDriver(
         for (final screenshot in screenshots) {
           final map = screenshot as Map<String, dynamic>;
           final name = map['screenshotName'] as String;
-          final bytes = base64Decode(map['bytes'] as String);
+          final bytes = (map['bytes'] as List<dynamic>).cast<int>();
           final file = File('screenshots/$name.png');
           await file.writeAsBytes(bytes);
           print('Saved: ${file.path}');
