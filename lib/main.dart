fbcbeeb236534325c160781878314e421435b981
pec0ra <basile.maret@proton.me>
2026-06-04 16:17:41 +0200

    Fix app name

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class SuspensionSetupApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Suspension Setup',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
